05ffd019034bf4db8aaa8af497e6cd4e26f13e0f
Yaroslav Grabovych <ygrabovych@gmail.com>
2023-10-03 22:46:55 +0300

    fixing

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,20 +4,36 @@ import Statistic from "./Statistic";
 import React, { useState } from "react";
 
 function App() {
-  const [feedback, setFeedBack] = useState({ good: 0, neutral: 0, bad: 0 });
+  // const [feedback, setFeedBack] = useState({ good: 0, neutral: 0, bad: 0 });
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
 
   const handleFeedback = (type) => {
-    console.log(`Feedback type: ${type}`);
-    console.log(feedback);
-    setFeedBack((prevState) => ({
-      ...prevState,
-      [type]: prevState[type] + 1,
-    }));
+    
+    switch (type) {
+      case "good":
+          setGood((state => state + 1))        
+        break;
+        case "neutral":
+          setNeutral((state => state + 1))        
+        break; 
+        case "bad":
+        setBad((state => state + 1))        
+      break;
+      default:
+        break;
+    }
+    // setFeedBack((prevState) => ({
+    //   ...prevState,
+    //   [type]: prevState[type] + 1,
+    // }));
   };
-  const total = feedback.good + feedback.neutral + feedback.bad;
+  const total = good + neutral + bad;
 
   const countPositiveFeedback = () => {
-    return total === 0 ? 0 : Math.round((feedback.good / total) * 100);
+    return total === 0 ? 0 : Math.round((good / total) * 100);
   };
   const positiveFeedback = countPositiveFeedback();
 
@@ -31,9 +47,9 @@ function App() {
       <h2>Statistic</h2>
       {total > 0 ? (
         <Statistic
-          good={feedback.good}
-          neutral={feedback.neutral}
-          bad={feedback.bad}
+          good={good}
+          neutral={neutral}
+          bad={bad}
           total={total}
           positiveFeedback={positiveFeedback}
         />
